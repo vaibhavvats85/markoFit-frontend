b07ac31a071ff017c5c832d8ca56b98a0f499ee4
VAIBHAV VATS <500040117@DDN.UPES.AC.IN>
2019-03-21 02:13:36 -0700

    server.js updated

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const path = require('path');
 app.use(express.statis(__dirname+'/dist'));
 app.listen(process.env.PORT|| 8080);
 app.get('/*', function(req, res) {
-    res.sendFile(path.join(__dirname + '/dist/index.html'));
+    res.sendFile(path.join(__dirname + '/dist/AngularStock/index.html'));
 });
 
 console.log('Console listening');
