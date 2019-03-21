9652d879caf001a79bf9e364d5f8a981cf1d2689
vaibhavvats85 <vaibhavvats85@gmail.com>
2019-03-21 02:18:26 -0700

    Update server.js

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const path = require('path');
 
-app.use(express.statis(__dirname+'/dist'));
+app.use(express.statis(__dirname+'/dist/AngularStock'));
 app.listen(process.env.PORT|| 8080);
 app.get('/*', function(req, res) {
     res.sendFile(path.join(__dirname + '/dist/AngularStock/index.html'));
